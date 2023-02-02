739fb720815cd2c71f3e9f650d5fca41f7a3692b
Md Rafiqul Islam <119554804+softic-rafiq@users.noreply.github.com>
2023-02-02 11:13:09 +0600

    User register done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,16 +2,49 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../config/dbConnection");
 
-router.get("/", (req, res) => {
-  const userQuery = "select * from users";
-  connection.query(userQuery, function (err, results, fields) {
-    if (err) {
-      console.log(err.message);
+router.post("/signup", (req, res) => {
+  let user = req.body;
+  const userQuery =
+    "select email,password,role,status from users where email=?";
+  connection.query(userQuery, [user.email], (err, results) => {
+    if (!err) {
+      // return res.json(results);
+      if (results.length <= 0) {
+        insertQuery =
+          "insert into users(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
+        connection.query(
+          insertQuery,
+          [user.name, user.contactNumber, user.email, user.password],
+          (err, results) => {
+            if (!err) {
+              return res.status(201).json({
+                message: "User registration successfull",
+              });
+            } else {
+              return res.status(500).json(err);
+            }
+          }
+        );
+      } else {
+        return res.status(400).json({
+          message: "Email already exist",
+        });
+      }
+      // console.log("Get all user", results);
     } else {
-      return res.json(results);
-      console.log("Get all user", results);
+      return res.status(500).json(err);
     }
   });
 });
 
+router.get("/", (req, res) => {
+  const userQuery = "select * from users";
+  connection.query(userQuery, (err, results) => {
+    if (!err) {
+      return res.status(200).json(results);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
 module.exports = router;
